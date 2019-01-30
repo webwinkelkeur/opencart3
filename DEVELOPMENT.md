d42958ebc9f8518874434af1155aca193326f155
Albert Peschar <albert@peschar.net>
2019-01-30 12:48:29 +0200

    Configure, document testing installation

---
+++ DEVELOPMENT.md
@@ -0,0 +1,4 @@
+Import the database for the testing install:
+
+    mysqladmin create wwk_oc3
+    mysql wwk_oc3 < opencart3.sql
---
+++ www/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test/webwinkelkeur/opencart3/www/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test/webwinkelkeur/opencart3/www/');
+
+// DIR
+define('DIR_APPLICATION', '/data/webwinkelkeur/opencart3/www/catalog/');
+define('DIR_SYSTEM', '/data/webwinkelkeur/opencart3/www/system/');
+define('DIR_IMAGE', '/data/webwinkelkeur/opencart3/www/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'wwk_oc3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
