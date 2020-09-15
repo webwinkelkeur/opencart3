c9380b0d4bd4fe722e8bf60c6009183e3e1f559a
tsanko <tsankohadzhiev@gmail.com>
2020-09-15 16:01:51 +0300

    Update sidebar script

--- upload/catalog/controller/extension/module/webwinkelkeur.php
+++ upload/catalog/controller/extension/module/webwinkelkeur.php
@@ -112,8 +112,7 @@ class ControllerExtensionModuleWebwinkelkeur extends Controller {
         }
 
         return
-            '<script>_webwinkelkeur_id=' . (int) $this->settings['shop_id'] . '</script>' .
-            '<script async src="https://' . $this->msg['APP_DOMAIN'] . '/js/sidebar.js"></script>';
+            sprintf("<script>(function(url,id){var script=document.createElement('script');script.async=true;script.src=url+'/sidebar.js?id='+id+'&c='+cachebuster(10,id);var ref=document.getElementsByTagName('script')[0];ref.parentNode.insertBefore(script,ref);function cachebuster(refreshMinutes,id){var now=Date.now();var interval=refreshMinutes*60e3;var shift=(Math.sin(id)||0)*interval;return Math.floor((now+shift)/interval);}})('https://%s',%d);</script>", $this->msg['APP_DOMAIN'], (int) $this->settings['shop_id']);
 
         return $o;
     }
